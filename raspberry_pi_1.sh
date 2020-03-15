a7911971e748b13baf2bdef61fba12f68a46cc91
Daniel Šíma <dan0nik192000@gmail.com>
2020-03-15 11:17:22 +0100

    update

--- raspberry_pi_1.sh
+++ raspberry_pi_1.sh
@@ -4,6 +4,13 @@
 # SETUP SCRIPT FOR RASPBERRY PI 1
 ################################################################################
 
+#BEFORE THIS DO MANUALLY:
+
+#git clone  --single-branch --branch raspberry_pi_1 https://github.com/DanielSima/dotfiles.git /home/pi/configs/raspberry_pi_1
+#sudo chmod 777 /home/pi/configs/raspberry_pi_1/raspberry_pi_1.sh
+#/home/pi/configs/raspberry_pi_1/raspberry_pi_1.sh
+
+#SCRIPT START
 # exit when any command fails
 set -e
 # keep track of the last executed command
@@ -19,44 +26,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #pivpn
 curl -L https://install.pivpn.io | bash
 ################################################################################
-#dotfiles
-################################################################################
-mkdir /home/pi/configs
-#nano
-ln -sf $(DIR)/nanorc /etc/nanorc
-#motd
-sudo rm /etc/update-motd.d
-sudo ln -s $(DIR)/update-motd.d /etc/update-motd.d
-sudo chmod -R 777 /etc/update-motd.d
-#homeassistant
-
-git clone https://github.com/DanielSima/home-assistant.git /home/win/configs/homeassistant
-#git clone https://github.com/DanielSima/home-assistant.git /home/win/configs/organizr
-#git clone https://github.com/DanielSima/home-assistant.git /home/win/configs/pihole
-
-
-
-
-
-
-
-#git clone --single-branch --branch testbranch  https://github.com/DanielSima/test.git /home/win/configs/tes2
-################################################################################
 #containers
 ################################################################################
 #homeassistant
 docker run --init -d --restart=unless-stopped --name homeassistant -e TZ=Europe/Prague \
--v /home/pi/configs/home-assistant:/config --net=host \
+-v /home/pi/configs/raspberry_pi_1/homeassistant:/config --net=host \
 homeassistant/raspberrypi3-homeassistant:stable
 
 #pihole
 docker run --init -d --restart=unless-stopped --name pihole -e TZ=Europe/Prague \
--v /home/pi/configs/pihole/pihole/:/etc/pihole -v /home/pi/configs/pihole/dnsmasq.d/:/etc/dnsmasq.d \
+-v /home/pi/configs/raspberry_pi_1/pihole/pihole:/etc/pihole -v /home/pi/configs/raspberry_pi_1/pihole/dnsmasq.d/:/etc/dnsmasq.d \
 -p 53:53/tcp -p 53:53/udp -p 80:80 -p 443:443 --dns=127.0.0.1 --dns=8.8.8.8 \
 pihole/pihole:latest
 
 #organizr
 docker run --init -d --restart unless-stopped --name organizr -e TZ=Europe/Prague \
--v /home/pi/configs/organizr:/config \
+-v /home/pi/configs/raspberry_pi_1/organizr:/config \
 -e PUID=1000 -e PGID=1000 -p 9983:80 \
 linuxserver/organizr
