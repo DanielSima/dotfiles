5a630412415d8ee3981c3720f13145b123342407
DanielSima <dan0nik192000@gmail.com>
2020-04-05 17:31:24 +0200

    home assistant moved from seperate repository

--- raspberry_pi_1.sh
+++ raspberry_pi_1.sh
@@ -25,19 +25,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ################################################################################
 #pivpn
 curl -L https://install.pivpn.io | bash
-pivpn add
+pivpn add #xx.duckdns.org
 ################################################################################
 #containers
 ################################################################################
 #homeassistant
-#sudo docker run --init -d --restart=unless-stopped --name homeassistant -e TZ=Europe/Prague \
-#-v /home/pi/configs/raspberry_pi_1/homeassistant:/config --net=host \
-#homeassistant/raspberrypi3-homeassistant:stable
+sudo docker run --init -d --restart=unless-stopped --name homeassistant -e TZ=Europe/Prague \
+-v /home/pi/configs/raspberry_pi_1/homeassistant:/config --net=host \
+homeassistant/raspberrypi3-homeassistant:stable
 
 #pihole
 sudo docker run -d --restart=unless-stopped --name pihole -e TZ=Europe/Prague \
 -v /home/pi/configs/raspberry_pi_1/pihole/pihole:/etc/pihole -v /home/pi/configs/raspberry_pi_1/pihole/dnsmasq.d/:/etc/dnsmasq.d \
---net=host --cap-add=NET_ADMIN --dns=127.0.0.1 --dns=8.8.8.8 -e ServerIP="192.168.0.152" \
+--net=host --cap-add=NET_ADMIN --dns=127.0.0.1 --dns=8.8.8.8 -e ServerIP="192.168.0.151" \
 pihole/pihole:latest
 ################################################################################
 #config
