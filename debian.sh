2d2ffcfc3361507d9b3e63f9a455d7cc5f56cc33
DanielSima <dan0nik192000@gmail.com>
2020-04-19 14:39:48 +0200

    script fixes, added readme

--- debian.sh
+++ debian.sh
@@ -4,20 +4,6 @@
 # SETUP SCRIPT FOR DEBIAN
 ################################################################################
 
-#BEFORE THIS DO MANUALLY:
-
-#ssh-keygen -R "192.168.."
-#ssh pi@192.168..
-#passwd
-#sudo passwd
-#sudo apt-get update && sudo apt-get upgrade
-#sudo apt install git
-#mkdir /home/pi/configs
-#git clone --single-branch --branch debian https://github.com/DanielSima/dotfiles.git /home/pi/configs/debian
-#sudo chmod 777 /home/pi/configs/debian/debian.sh
-#/home/pi/configs/debian/debian.sh
-
-#SCRIPT START
 # exit when any command fails
 set -e
 # keep track of the last executed command
@@ -25,18 +11,22 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 #save current dir, the cloned repo location
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+#save the user, works even when using sudo
+USER="$(who am i | awk '{print $1}')"
 ################################################################################
 #config
 ################################################################################
 sudo timedatectl set-timezone Europe/Prague
+#git
+git config --global user.email "dan0nik192000@gmail.com"
+git config --global user.name "DanielSima"
 ################################################################################
 #packages
 ################################################################################
 sudo apt-get install -y samba iotop iftop htop smartmontools nano neofetch
 #docker
 curl https://get.docker.com | sh
-sudo usermod -aG docker pi
 #glances
 curl -L https://bit.ly/glances | /bin/bash
 ################################################################################
@@ -54,14 +44,11 @@ sudo systemctl start glances
 sudo rm /etc/motd
 sudo rm -r /etc/update-motd.d
 sudo ln -s $(DIR)/update-motd.d /etc/update-motd.d
-sudo chmod -R 777 /etc/update-motd.d
+sudo chmod +r /etc/update-motd.d
 #bash
-sudo ln -sf $(DIR)/bash/.bash_functions /home/pi/.bash_functions
-sudo ln -sf $(DIR)/bash/.bash_aliases/home/pi/.bash_aliases
-sudo ln -sf $(DIR)/bash/.bashrc /home/pi/.bashrc
+sudo ln -sf $(DIR)/bash/.bash_functions /home/$(USER)/.bash_functions
+sudo ln -sf $(DIR)/bash/.bash_aliases/home/$(USER)/.bash_aliases
+sudo ln -sf $(DIR)/bash/.bashrc /home/$(USER)/.bashrc
 #samba
 sudo ln -sf $(DIR)/smb.conf /etc/samba/smb.conf
 sudo systemctl restart smbd
-#git
-git config --global user.email "dan0nik192000@gmail.com"
-git config --global user.name "DanielSima"
