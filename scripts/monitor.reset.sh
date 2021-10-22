30b4262f29810ff4dec958b4f9f1fa1b2ca8d1d4
DanielSima <dan0nik192000@gmail.com>
2021-10-22 09:02:24 +0200

    xrandr did not worked perfectly, this should solve it

--- scripts/monitor.reset.sh
+++ scripts/monitor.reset.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-xrandr
+
+multi(){
+    xrandr --output eDP-1 --off
+    xrandr --output DP-1 --mode 1920x1080 --primary
+    xrandr --output HDMI-1 --mode 1920x1080 --left-of DP-1
+}
+
+single(){
+    xrandr --output eDP-1 --mode 1920x1080 --primary
+}
+
+if xrandr --listmonitors | grep "HDMI" > /dev/null 2>&1; then
+    multi
+else
+    single
+fi
